81b15b414afb19493e33b9c1e2684db043e2d4d6
Mikhail Dombrovsky <madombrovsky@gmail.com>
2025-08-16 00:15:36 +0300

    pause menu fix, bug soundMixer fix

--- Assets/Scripts/Managers/SoundMixerManager.cs
+++ Assets/Scripts/Managers/SoundMixerManager.cs
@@ -8,7 +8,7 @@ public class SoundMixerManager : MonoBehaviour
     public void SetMasterVolume(float level)
     {
         //audioMixer.SetFloat("masterVolume", level);
-        audioMixer.SetFloat("MasterVolume", Mathf.Log10(level) * 20); // Convert linear volume to logarithmic scale
+        audioMixer.SetFloat("masterVolume", Mathf.Log10(level) * 20); // Convert linear volume to logarithmic scale
     }
     public void SetSoundFXVolume(float level)
     {
